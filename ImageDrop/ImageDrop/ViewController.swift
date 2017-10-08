befe587499198b5b4c7ba340d02fed78d1678321
Harshita <hc318@outlook.com>
2017-10-08 23:55:45 +0530

    add example scenario

---
+++ ImageDrop/ImageDrop/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ImageDrop
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var topImageView: UIImageView!
+    @IBOutlet var bottomImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
